485d413f18cb2b66cc977819ad1e5d00566fa134
Zerphyy <s1146282@student.windesheim.nl>
2023-03-16 14:07:16 +0100

    temp commit - fixing javascript for checkers, draggable tests, list final touches before v1

--- Setup/wwwroot/js/testing.js
+++ Setup/wwwroot/js/testing.js
@@ -4,10 +4,11 @@
     }
 }
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('click', () => {
     const createGameButton = document.querySelector('#create-game-button');
     const t = new testing();
     createGameButton.addEventListener('click', t.test);
+    createGameButton.innerHTML = "Test worked! :)";
 });
 
 //use this class to see how razor -> javascript works. check out the Create Game button in Dammen->Index
