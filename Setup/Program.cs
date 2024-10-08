12d4f1d26c9923b438583764376c4c1f8b5d53da
Zerphyy <s1146282@student.windesheim.nl>
2024-10-08 21:22:48 +0200

    changed login duration
    tried to fix promoted captures, not working yet.
    remake removing enemy pieces

--- Setup/Program.cs
+++ Setup/Program.cs
@@ -12,7 +12,7 @@ builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationSc
     .AddCookie(option =>
     {
         option.LoginPath = "/Access/Login";
-        option.ExpireTimeSpan = TimeSpan.FromMinutes(20);
+        option.ExpireTimeSpan = TimeSpan.FromMinutes(60);
     });
 
 
