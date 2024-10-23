627dc8a16e331d29974c73a341d381a7284313f4
Zerphyy <s1146282@student.windesheim.nl>
2024-10-23 19:03:05 +0200

    Added Cypress tests to test the major functionality of the checkers game
    major functionality only consists of normal moves, because the amount of time you have a promoted piece is far less than the time you have a normal piece.
    functionality tested:
    - making a normal move
    - making a capture move
    - making a consecutive capture move

--- TestShowcase/CypressFrontEndTests/e2e/spec1.cy.js
+++ TestShowcase/CypressFrontEndTests/e2e/spec1.cy.js
@@ -1,13 +1,35 @@
 let needsLogin = false;
+let testUser = {
+    name: 'TestUser',
+    email: 'testmail@example.com',
+    password: 'testUser123!',
+};
 
+let realUser = {
+    name: 'RealUser',
+    email: 'kevinspijker@kpnmail.nl',
+    password: 'Plusklas01!',
+};
+let currentUser;
+const setupUserSession = (user) => {
+    cy.session(user.name, () => {
+        cy.login(user.email, user.password, true);
+        cy.visit('http://145.44.234.91');
+        const today = new Date();
+        const date = today.getDate() + '-' + (today.getMonth() + 1) + '-' + today.getFullYear();
+        const time = today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds();
+
+        cy.window().then((win) => {
+            win.localStorage.setItem('gdpr-consent-choice', 'accept');
+            win.localStorage.setItem('time-of-interaction', JSON.stringify({ datum: date, tijd: time }));
+        });
+        cy.contains('button', 'Ja, natuurlijk').click();
+    });
+};
+Cypress.on('uncaught:exception', () => { return false })
 describe('All Showcase tests for the frontend using Cypress', () => {
     it('Visits the Showcase', () => {
         cy.visit('http://145.44.234.91');
-    });
-
-    it('Accepts the GDPR', () => {
-        cy.visit('http://145.44.234.91');
-        cy.contains('button', 'Ja, natuurlijk').click();
     });
 
     it('cant login without giving account creds', () => {
@@ -21,30 +43,13 @@ describe('All Showcase tests for the frontend using Cypress', () => {
     });
 });
 describe('Logged-in User Tests', () => {
-    let testUser = {
-        name: 'TestUser',
-        email: 'testmail@example.com',
-        password: 'testUser123!',
-    };
-
-    let realUser = {
-        name: 'RealUser',
-        email: 'kevinspijker@kpnmail.nl',
-        password: 'Plusklas01!',
-    };
-
-    let currentUser;
-
     beforeEach(() => {
         if (currentUser === testUser.name) {
-            cy.session(testUser.name, () => {
-                cy.login(testUser.email, testUser.password, testUser, true, true);
-            });
+            setupUserSession(testUser);
         } else if (currentUser === realUser.name) {
-            cy.session(realUser.name, () => {
-                cy.login(realUser.email, realUser.password, realUser, true, true);
-            });
+            setupUserSession(realUser);
         }
+
     });
 
     it('should log in as TestUser and show the Privacy page', () => {
@@ -91,10 +96,22 @@ describe('Logged-in User Tests', () => {
         cy.contains('a', 'Maak spel aan').click();
         cy.get('input[placeholder="Checkers is amazing!"]').type('TestGame');
         cy.get('input[type="Submit"]').click();
+        currentUser = realUser.name;
+    });
+    it('should log in as RealUser and join TestUser dammen game', () => {
+        cy.visit('http://145.44.234.91');
+        cy.contains('a', 'Dammen!').click();
+        cy.get('table')
+            .contains('td', 'TestGame')
+            .parents('tr')
+            .find('button')
+            .contains('Meedoen')
+            .click();
+        currentUser = testUser.name;
     });
 
     it('should log in as TestUser and log out', () => {
-        currentUser = testUser.name;
+        
         cy.visit('http://145.44.234.91');
         cy.contains('a', 'Uitloggen').click();
     });
@@ -104,5 +121,444 @@ describe('Logged-in User Tests', () => {
         cy.contains('a', 'Privacy').click();
     });
 
+});
+describe('Dammen tests', () => {
+    beforeEach(() => {
+        if (currentUser === testUser.name) {
+            setupUserSession(testUser);
+        } else if (currentUser === realUser.name) {
+            setupUserSession(realUser);
+        }
+
+    });
+    it('should log in as TestUser and view his dammen game', () => {
+        cy.visit('http://145.44.234.91');
+        cy.contains('a', 'Dammen!').click();
+        cy.get('table')
+            .contains('td', 'TestGame')
+            .parents('tr')
+            .find('a')
+            .contains('Bekijken')
+            .click();
+    });
+    it('should log in as TestUser and make a move in his dammen game', () => {
+        cy.visit('http://145.44.234.91');
+        cy.contains('a', 'Dammen!').click();
+        cy.get('table')
+            .contains('td', 'TestGame')
+            .parents('tr')
+            .find('a')
+            .contains('Bekijken')
+            .click();
+        cy.wait(5000);
+        cy.get('#shadowHost').then((shadowHost) => {
+            // Access the shadow root
+            const shadowRoot = shadowHost[0].shadowRoot;
+
+            // Use querySelector to find the square and then the piece inside
+            let square = shadowRoot.querySelector('#square21');
+            let piece;
+            if (square) {
+                // Check if the square is visible
+                expect(square).to.be.visible;
+
+                // Find the inner piece
+                piece = square.querySelector('#piece1');
+                if (piece) {
+                    // Check if the piece is visible
+                    expect(piece).to.be.visible;
+
+                    // Click the inner piece
+                    piece.click();
+                } else {
+                    throw new Error('Piece not found');
+                }
+            } else {
+                throw new Error('Square not found');
+            }
+            square = shadowRoot.querySelector('#square30');
+            if (square) {
+                // Check if the square is visible
+                expect(square).to.be.visible;
+
+                // Find the inner piece
+                piece = square.querySelector('.grey-circle');
+                if (piece) {
+                    // Check if the piece is visible
+                    expect(piece).to.be.visible;
+
+                    // Click the inner piece
+                    piece.click();
+                } else {
+                    throw new Error('Piece not found');
+                }
+            } else {
+                throw new Error('Square not found');
+            }
+        });
+        cy.wait(5000);
+        currentUser = realUser.name;
+    });
+    it('should log in as RealUser and make a move in his dammen game', () => {
+        cy.visit('http://145.44.234.91');
+        cy.contains('a', 'Dammen!').click();
+        cy.get('table')
+            .contains('td', 'TestGame')
+            .parents('tr')
+            .find('a')
+            .contains('Bekijken')
+            .click();
+        cy.wait(5000);
+        cy.get('#shadowHost').then((shadowHost) => {
+            // Access the shadow root
+            const shadowRoot = shadowHost[0].shadowRoot;
+
+            // Use querySelector to find the square and then the piece inside
+            let square = shadowRoot.querySelector('#square44');
+            let piece;
+            if (square) {
+                // Check if the square is visible
+                expect(square).to.be.visible;
+
+                // Find the inner piece
+                piece = square.querySelector('#piece2');
+                if (piece) {
+                    // Check if the piece is visible
+                    expect(piece).to.be.visible;
+
+                    // Click the inner piece
+                    piece.click();
+                } else {
+                    throw new Error('Piece not found');
+                }
+            } else {
+                throw new Error('Square not found');
+            }
+            square = shadowRoot.querySelector('#square37');
+            if (square) {
+                // Check if the square is visible
+                expect(square).to.be.visible;
+
+                // Find the inner piece
+                piece = square.querySelector('.grey-circle');
+                if (piece) {
+                    // Check if the piece is visible
+                    expect(piece).to.be.visible;
+
+                    // Click the inner piece
+                    piece.click();
+                } else {
+                    throw new Error('Piece not found');
+                }
+            } else {
+                throw new Error('Square not found');
+            }
+        });
+        cy.wait(5000);
+        currentUser = testUser.name;
+    });
+    it('should log in as TestUser and make a capture move in his dammen game', () => {
+        cy.visit('http://145.44.234.91');
+        cy.contains('a', 'Dammen!').click();
+        cy.get('table')
+            .contains('td', 'TestGame')
+            .parents('tr')
+            .find('a')
+            .contains('Bekijken')
+            .click();
+        cy.wait(5000);
+        cy.get('#shadowHost').then((shadowHost) => {
+            // Access the shadow root
+            const shadowRoot = shadowHost[0].shadowRoot;
+
+            // Use querySelector to find the square and then the piece inside
+            let square = shadowRoot.querySelector('#square30');
+            let piece;
+            if (square) {
+                // Check if the square is visible
+                expect(square).to.be.visible;
+
+                // Find the inner piece
+                piece = square.querySelector('#piece1');
+                if (piece) {
+                    // Check if the piece is visible
+                    expect(piece).to.be.visible;
+
+                    // Click the inner piece
+                    piece.click();
+                } else {
+                    throw new Error('Piece not found');
+                }
+            } else {
+                throw new Error('Square not found');
+            }
+            square = shadowRoot.querySelector('#square44');
+            if (square) {
+                // Check if the square is visible
+                expect(square).to.be.visible;
+
+                // Find the inner piece
+                piece = square.querySelector('.grey-circle');
+                if (piece) {
+                    // Check if the piece is visible
+                    expect(piece).to.be.visible;
+
+                    // Click the inner piece
+                    piece.click();
+                } else {
+                    throw new Error('Piece not found');
+                }
+            } else {
+                throw new Error('Square not found');
+            }
+        });
+        cy.wait(5000);
+        currentUser = realUser.name;
+    });
+    it('should log in as RealUser and make a non-capture-forced move in his dammen game', () => {
+        cy.visit('http://145.44.234.91');
+        cy.contains('a', 'Dammen!').click();
+        cy.get('table')
+            .contains('td', 'TestGame')
+            .parents('tr')
+            .find('a')
+            .contains('Bekijken')
+            .click();
+        cy.wait(5000);
+        cy.get('#shadowHost').then((shadowHost) => {
+            // Access the shadow root
+            const shadowRoot = shadowHost[0].shadowRoot;
+
+            // Use querySelector to find the square and then the piece inside
+            let square = shadowRoot.querySelector('#square42');
+            let piece;
+            if (square) {
+                // Check if the square is visible
+                expect(square).to.be.visible;
+
+                // Find the inner piece
+                piece = square.querySelector('#piece2');
+                if (piece) {
+                    // Check if the piece is visible
+                    expect(piece).to.be.visible;
+
+                    // Click the inner piece
+                    piece.click();
+                } else {
+                    throw new Error('Piece not found');
+                }
+            } else {
+                throw new Error('Square not found');
+            }
+            square = shadowRoot.querySelector('#square33');
+            if (square) {
+                // Check if the square is visible
+                expect(square).to.be.visible;
+
+                // Find the inner piece
+                piece = square.querySelector('.grey-circle');
+                if (piece) {
+                    // Check if the piece is visible
+                    expect(piece).to.be.visible;
+
+                    // Click the inner piece
+                    piece.click();
+                } else {
+                    throw new Error('Piece not found');
+                }
+            } else {
+                throw new Error('Square not found');
+            }
+        });
+        cy.wait(5000);
+        currentUser = testUser.name;
+    });
+    it('should log in as TestUser and make a non-capture-forced move in his dammen game', () => {
+        cy.visit('http://145.44.234.91');
+        cy.contains('a', 'Dammen!').click();
+        cy.get('table')
+            .contains('td', 'TestGame')
+            .parents('tr')
+            .find('a')
+            .contains('Bekijken')
+            .click();
+        cy.wait(5000);
+        cy.get('#shadowHost').then((shadowHost) => {
+            // Access the shadow root
+            const shadowRoot = shadowHost[0].shadowRoot;
+
+            // Use querySelector to find the square and then the piece inside
+            let square = shadowRoot.querySelector('#square23');
+            let piece;
+            if (square) {
+                // Check if the square is visible
+                expect(square).to.be.visible;
+
+                // Find the inner piece
+                piece = square.querySelector('#piece1');
+                if (piece) {
+                    // Check if the piece is visible
+                    expect(piece).to.be.visible;
+
+                    // Click the inner piece
+                    piece.click();
+                } else {
+                    throw new Error('Piece not found');
+                }
+            } else {
+                throw new Error('Square not found');
+            }
+            square = shadowRoot.querySelector('#square30');
+            if (square) {
+                // Check if the square is visible
+                expect(square).to.be.visible;
+
+                // Find the inner piece
+                piece = square.querySelector('.grey-circle');
+                if (piece) {
+                    // Check if the piece is visible
+                    expect(piece).to.be.visible;
+
+                    // Click the inner piece
+                    piece.click();
+                } else {
+                    throw new Error('Piece not found');
+                }
+            } else {
+                throw new Error('Square not found');
+            }
+        });
+        cy.wait(5000);
+        currentUser = realUser.name;
+    });
+    it('should log in as RealUser and make the first move in a double move in his dammen game', () => {
+        cy.visit('http://145.44.234.91');
+        cy.contains('a', 'Dammen!').click();
+        cy.get('table')
+            .contains('td', 'TestGame')
+            .parents('tr')
+            .find('a')
+            .contains('Bekijken')
+            .click();
+        cy.wait(5000);
+        cy.get('#shadowHost').then((shadowHost) => {
+            // Access the shadow root
+            const shadowRoot = shadowHost[0].shadowRoot;
+
+            // Use querySelector to find the square and then the piece inside
+            let square = shadowRoot.querySelector('#square51');
+            let piece;
+            if (square) {
+                // Check if the square is visible
+                expect(square).to.be.visible;
+
+                // Find the inner piece
+                piece = square.querySelector('#piece2');
+                if (piece) {
+                    // Check if the piece is visible
+                    expect(piece).to.be.visible;
+
+                    // Click the inner piece
+                    piece.click();
+                } else {
+                    throw new Error('Piece not found');
+                }
+            } else {
+                throw new Error('Square not found');
+            }
+            square = shadowRoot.querySelector('#square37');
+            if (square) {
+                // Check if the square is visible
+                expect(square).to.be.visible;
+
+                // Find the inner piece
+                piece = square.querySelector('.grey-circle');
+                if (piece) {
+                    // Check if the piece is visible
+                    expect(piece).to.be.visible;
+
+                    // Click the inner piece
+                    piece.click();
+                } else {
+                    throw new Error('Piece not found');
+                }
+            } else {
+                throw new Error('Square not found');
+            }
+
+            cy.wait(5000);
+        });
+    });
+    it('should log in as RealUser and make the second move in a double move in his dammen game', () => {
+        cy.visit('http://145.44.234.91');
+        cy.contains('a', 'Dammen!').click();
+        cy.get('table')
+            .contains('td', 'TestGame')
+            .parents('tr')
+            .find('a')
+            .contains('Bekijken')
+            .click();
+        cy.wait(5000);
+        cy.get('#shadowHost').then((shadowHost) => {
+            // Access the shadow root
+            const shadowRoot = shadowHost[0].shadowRoot;
+
+            // Use querySelector to find the square and then the piece inside
+            let square = shadowRoot.querySelector('#square37');
+            let piece;
+            if (square) {
+                // Check if the square is visible
+                expect(square).to.be.visible;
+
+                // Find the inner piece
+                piece = square.querySelector('#piece2');
+                if (piece) {
+                    // Check if the piece is visible
+                    expect(piece).to.be.visible;
+
+                    // Click the inner piece
+                    piece.click();
+                } else {
+                    throw new Error('Piece not found');
+                }
+            } else {
+                throw new Error('Square not found');
+            }
+            square = shadowRoot.querySelector('#square23');
+            if (square) {
+                // Check if the square is visible
+                expect(square).to.be.visible;
+
+                // Find the inner piece
+                piece = square.querySelector('.grey-circle');
+                if (piece) {
+                    // Check if the piece is visible
+                    expect(piece).to.be.visible;
+
+                    // Click the inner piece
+                    piece.click();
+                } else {
+                    throw new Error('Piece not found');
+                }
+            } else {
+                throw new Error('Square not found');
+            }
+
+            cy.wait(5000);
+        });
+        currentUser = testUser.name;
+    });
+    it('should remove TestUsers game', () => {
+        cy.visit('http://145.44.234.91');
+        cy.contains('a', 'Dammen!').click();
+        cy.get('h2').contains('Jouw games')
+            .next('table')
+            .find('td')
+            .contains('TestGame')
+            .parents('tr')
+            .find('button')
+            .contains('verwijderen')
+            .click();
+    })
 });
 
